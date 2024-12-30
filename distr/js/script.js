793a3f2743781d91db39352f042d3361ef33d954
dave-teg <tegdaw7@gmail.com>
2024-12-30 18:36:27 +0300

    Minor changes

--- distr/js/script.js
+++ distr/js/script.js
@@ -159,7 +159,7 @@ const displayComputerChoice = (computerChoice) => {
 
 const createComputerImg = (icon, parent) => {
   const cpImg = document.createElement("img");
-  cpImg.src = `/dist/images/${icon}.png`
+  cpImg.src = `/distr/images/${icon}.png`
   cpImg.alt = `${icon}`;
   parent.appendChild(cpImg);
 }
